e9ffaf5d6982f76f8efc9f945379903da4f51733
Aleksanian <>
2021-05-28 17:20:28 +0300

    some more minor changes

---
+++ Assets/Scripts/Game/GameEntry.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+namespace Game
+{
+    public class GameEntry : MonoBehaviour
+    {
+        private void Awake()
+        {
+            
+        }
+    }
+}
